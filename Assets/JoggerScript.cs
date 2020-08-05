5ef65abc98e213ed13e8476409c825fb91b1c568
TylerLockhart <tylock@umich.edu>
2020-08-05 14:26:54 -0400

    Add files via upload

---
+++ Assets/JoggerScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JoggerScript : MonoBehaviour
+{
+    //Things to add to player script
+    public Transform transform;
+    public Rigidbody rb;
+    public LocationScript currentLocation;
+    public LocationScript home;
+    //stress
+    public int maxStress;
+    public int currentStress;
+
+    //method for returning home
+    //when this method executes we need to do a few different things. We first need to return to the home location. We also need to increment the maximum stress and set current stress = max stress
+    //all visitedToday tags need to be set to uncalled
+    
+    
+    IEnumerator switchLocation (LocationScript destination){
+        //if we have arrived at the destination, call the function that starts a new event
+
+        float differenceX = 0;
+        float differenceY = 0;
+        float differenceZ = 0;
+        differenceX = (destination.gameObject.transform.position.x - currentLocation.gameObject.transform.position.x);
+        differenceY = (destination.gameObject.transform.position.y - currentLocation.gameObject.transform.position.y);
+        differenceZ = (destination.gameObject.transform.position.z - currentLocation.gameObject.transform.position.z);
+        rb.velocity = new Vector3(differenceX,differenceY,differenceZ);
+        yield return new WaitForSeconds(1f);
+        rb.velocity = new Vector3(0,0,0);
+        currentLocation = destination;
+        //Call event coroutines
+    }
+
+    void ReturnHome (){
+        switchLocation(home);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(home.gameObject.transform.position.x, home.gameObject.transform.position.y + 0.4f, home.gameObject.transform.position.z);
+    } 
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey("d")){
+            StartCoroutine(switchLocation(currentLocation.adjacentD));
+        }
+        if(Input.GetKey("a")){
+            StartCoroutine(switchLocation(currentLocation.adjacentA));
+        }
+    }
+}
---
+++ Assets/LocationScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocationScript : MonoBehaviour
+{
+    // to keep track of the actual location of the event
+    public Transform location;
+    
+    //4 different Location objects for the 4 different possible adjacent events
+    public LocationScript adjacentW;
+    public LocationScript adjacentA;
+    public LocationScript adjacentS;
+    public LocationScript adjacentD;
+    
+    //to keep track of what stage in the event process we are in. Also, stage 1 or 2 indicates which aftermath we get
+    public int stage = 0; 
+    
+    //the cost for selecting each option
+    public int option1Cost;
+    public int option2Cost;
+
+    //an object to keep track of all the text options: prompt, option 1, option 2, result 1, result 2, aftermath 1, aftermath 2
+    public string prompt;
+    public string option1;
+    public string option2;
+    public string result1;
+    public string result2;
+    public string aftermath1;
+    public string aftermath2;
+
+    //to add: a method that takes us to the event screen, and sets the visitedToday tag to true
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
